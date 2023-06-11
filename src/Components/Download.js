013f8ecb9376599b231347ac488b3c81d4ae919f
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-06-11 13:04:38 -0300

    remove underline

--- src/Components/Download.js
+++ src/Components/Download.js
@@ -14,7 +14,7 @@ const Download = () => {
             Aqui você pode baixar a versão mais recente do nosso aplicativo.
           </h1>
         </div>
-        <a href="https://objectstorage.sa-vinhedo-1.oraclecloud.com/n/axs7rpnviwd0/b/manga-easy-apks/o/manga-easy-latest.apk" target="_blank" rel="noreferrer">
+        <a href="https://objectstorage.sa-vinhedo-1.oraclecloud.com/n/axs7rpnviwd0/b/manga-easy-apks/o/manga-easy-latest.apk" target="_blank" rel="noreferrer"  style={{ textDecoration: "none" }} >
         <button className="secondary-button">
           Baixar Agora <FiArrowRight />{" "}
         </button>
