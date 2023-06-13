30ef15ec18e7440c1c61d4a85b7e940a040604c0
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-06-12 23:54:05 -0300

    fetch toggle/search

--- src/Components/Download.js
+++ src/Components/Download.js
@@ -1,7 +1,48 @@
 import React from "react";
 import { FiArrowRight } from "react-icons/fi";
 
+const apiUrl = 'https://micro-config.lucas-cm.com.br/v1/toggle/search';
+
+function fetchToggles() {
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const versionElement = document.getElementById('version');
+      const linkDownloadElement = document.getElementById('link-download');
+
+      // Find the item with the name "currentVersionApp"
+      const currentVersion = data.data.find(item => item.name === "currentVersionApp");
+
+      // Find the item with the name "currentVersionApp"
+      const currentDownloadLink = data.data.find(item => item.name === "linkDownloadApp");
+
+      if (currentVersion) {
+        // Get the value from the item and update the content of the element
+        versionElement.textContent = `Versão atual: ${currentVersion.value}`;
+      } else {
+        versionElement.textContent = 'Erro ao buscar a versão';
+      }
+
+      if (currentDownloadLink) {
+        // Get the value from the item and update the href attribute of the anchor element
+        if (currentDownloadLink.value) {
+          linkDownloadElement.href = currentDownloadLink.value;
+          linkDownloadElement.classList.remove('disabled'); // Remove the "disabled" class
+        }
+      } else {
+        linkDownloadElement.classList.add('disabled'); // Add the "disabled" class
+      }
+
+    })
+    .catch(error => {
+      console.error('Erro ao buscar a versão:', error);
+    });
+}
+
+
+
 const Download = () => {
+  fetchToggles();
   return (
     <div className="download-section-container">
       <div className="download-section-image-container">
@@ -14,14 +55,19 @@ const Download = () => {
             Aqui você pode baixar a versão mais recente do nosso aplicativo.
           </h1>
         </div>
-        <a href="https://objectstorage.sa-vinhedo-1.oraclecloud.com/n/axs7rpnviwd0/b/manga-easy-apks/o/manga-easy-latest.apk" target="_blank" rel="noreferrer"  style={{ textDecoration: "none" }} >
-        <button className="secondary-button">
-          Baixar Agora <FiArrowRight />{" "}
-        </button>
-        </a>
+        <div className="download-versao">
+          <a id="link-download"  href="https://google.com" target="_blank" rel="noreferrer" style={{ textDecoration: "none" }} >
+            <button className="secondary-button">
+              Baixar Agora <FiArrowRight />{" "}
+            </button>
+          </a>
+          <p id="version">Versão atual: Carregando...</p>
+        </div>
+
       </div>
     </div>
   );
 };
+
 
 export default Download;
